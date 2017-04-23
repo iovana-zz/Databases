a83076b15f793c695dd2748117645d34553ba179
iovana <ip4g14@soton.ac.uk>
2017-04-23 21:55:35 +0100

    Broke it

--- sjdb/src/sjdb/SJDB.java
+++ sjdb/src/sjdb/SJDB.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package sjdb;
 import java.io.*;
@@ -19,15 +19,15 @@ public class SJDB {
 		Catalogue cat = new Catalogue();
 		CatalogueParser catParser = new CatalogueParser(catFile, cat);
 		catParser.parse();
-		
+
 		// read stdin, parse, and build canonical query plan
 		QueryParser queryParser = new QueryParser(cat, new InputStreamReader(System.in));
 		Operator plan = queryParser.parse();
-				
+
 		// create estimator visitor and apply it to canonical plan
 		//Estimator est = new Estimator();
 		//plan.accept(est);
-		
+
 		// create optimised plan
 		//Optimiser opt = new Optimiser(cat);
 		//Operator optPlan = opt.optimise(plan);
