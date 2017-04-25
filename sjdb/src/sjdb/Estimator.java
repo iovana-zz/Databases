53d97ba3a9a0f6c6047bd80f3d2980168bc0c191
iovana <ip4g14@soton.ac.uk>
2017-04-25 21:42:11 +0100

    New Estimator

--- sjdb/src/sjdb/Estimator.java
+++ sjdb/src/sjdb/Estimator.java
@@ -1,7 +1,6 @@
 package sjdb;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Estimator implements PlanVisitor {
@@ -51,7 +50,9 @@ public class Estimator implements PlanVisitor {
 	}
 
 	public void visit(Select op) {
+		// get the predicate value
 		Predicate pred = op.getPredicate();
+		// get the input relation
 		Relation input = op.getInput().getOutput();
 		float left_value_count = 0;
 		float right_value_count = 0;
@@ -60,47 +61,50 @@ public class Estimator implements PlanVisitor {
 		Attribute left_attr = pred.getLeftAttribute();
 		Attribute right_attr, attr1, attr2;
 
-		//System.out.println("LEFT ATTR IS: " + left_attr);
-		System.out.println("PRED IS: " + pred);
-
-		// loop through the attribute list and find the value count for the left
-		// attribute
+		// loop through the attribute list and find the value count for the left attribute
 		List<Attribute> attributes = input.getAttributes();
-		left_attr = findAttribute(left_attr, attributes);
-
-//		for(int i=0; i < attributes.size(); i++) {
-////			System.out.println(attributes.get(i));
-//		}
-		//	System.out.println(left_attr);
-		if (left_attr != null) {
+		if(findAttribute(left_attr, attributes)!=null) {
+			left_attr = findAttribute(left_attr, attributes);
 			left_value_count = left_attr.getValueCount();
 		}
 
 		// true if the predicate is of the form attr=value
 		if (pred.equalsValue()) {
-			String value = pred.getRightValue();
-			rel_count = input.getTupleCount() / left_value_count;
-			output = new Relation((int) rel_count);
+			// String value = pred.getRightValue();
+			rel_count = input.getTupleCount()/left_value_count;
+			output = new Relation((int)rel_count);
+			//set the value count to 1
 			attr1 = new Attribute(left_attr.getName(), 1);
+			for(int i = 0; i < attributes.size(); i++) {
+				if(!attributes.get(i).equals(left_attr)) {
+					output.addAttribute(attributes.get(i));
+				}
+			}
 			output.addAttribute(attr1);
 		} else {
+			// get the right attribute
 			right_attr = pred.getRightAttribute();
+			//find it in the relation
 			right_attr = findAttribute(right_attr, attributes);
-			if (right_attr != null) {
-				right_value_count = right_attr.getValueCount();
-				float max_value = Math.max(left_value_count, right_value_count);
-				rel_count = input.getTupleCount() / max_value;
-				output = new Relation((int) rel_count);
-				float min_value = Math.min(left_value_count, right_value_count);
-				attr1 = new Attribute(left_attr.getName(), (int) min_value);
-				attr2 = new Attribute(right_attr.getName(), (int) min_value);
-				output.addAttribute(attr1);
-				output.addAttribute(attr2);
-			} else {
-				output = new Relation(0);
+			//get the value count
+			right_value_count = right_attr.getValueCount();
+			float max_value = Math.max(left_value_count, right_value_count);
+			rel_count = input.getTupleCount()/max_value;
+			output = new Relation((int)rel_count);
+			float min_value = Math.min(left_value_count, right_value_count);
+			// check also if V(R, A) <= T(R)
+			min_value = Math.min(rel_count, min_value);
+			// create left and right attributes
+			attr1 = new Attribute(left_attr.getName(), (int) min_value);
+			attr2 = new Attribute(right_attr.getName(), (int) min_value);
+			for(int i = 0; i < attributes.size(); i++) {
+				if(!attributes.get(i).equals(left_attr) && !attributes.get(i).equals(right_attr)) {
+					output.addAttribute(attributes.get(i));
+				}
 			}
+			output.addAttribute(attr1);
+			output.addAttribute(attr2);
 		}
-
 		op.setOutput(output);
 		(new Inspector()).visit(op);
 	}
@@ -120,9 +124,13 @@ public class Estimator implements PlanVisitor {
 		Operator left_op = op.getLeft();
 		Operator right_op = op.getRight();
 
+		Relation left_rel = left_op.getOutput();
+		Relation right_rel = right_op.getOutput();
+
 		// calculate the tuple count of the output relation
-		float left_tuple_count = left_op.getOutput().getTupleCount();
-		float right_tuple_count = right_op.getOutput().getTupleCount();
+		float left_tuple_count = left_rel.getTupleCount();
+		float right_tuple_count = right_rel.getTupleCount();
+		// T(R x S) = T(R)T(S)
 		float tuple_count = left_tuple_count * right_tuple_count;
 
 		// create new relation
@@ -131,10 +139,10 @@ public class Estimator implements PlanVisitor {
 		List<Attribute> right_attr = right_op.getOutput().getAttributes();
 
 		// and add the attributes
-		for (int i = 0; i < left_attr.size(); i++) {
+		for(int i = 0; i <  left_attr.size(); i++) {
 			output.addAttribute(left_attr.get(i));
 		}
-		for (int i = 0; i < right_attr.size(); i++) {
+		for(int i = 0; i <  right_attr.size(); i++) {
 			output.addAttribute(right_attr.get(i));
 		}
 		op.setOutput(output);
@@ -146,13 +154,53 @@ public class Estimator implements PlanVisitor {
 		Operator right_op = op.getRight();
 		Predicate pred = op.getPredicate();
 
-		// tuple count = T(R)T(S)/max(V(R,A),V(S,B))
-		System.out.println(left_op);
-		System.out.println(right_op);
-		System.out.println(pred);
+		Relation left_rel = left_op.getOutput();
+		Relation right_rel = right_op.getOutput();
 
-		Relation output = new Relation(0);
+		// get tuple count of relation A and B
+		float left_tuple_count = left_rel.getTupleCount();
+		float right_tuple_count = right_rel.getTupleCount();
+
+		// get the left and right attributes from the predicate
+		Attribute left_attr = pred.getLeftAttribute();
+		Attribute right_attr = pred.getRightAttribute();
+
+		// get the attributes from relation A and B
+		List<Attribute> left_attributes = left_rel.getAttributes();
+		List<Attribute> right_attributes = right_rel.getAttributes();
+
+		// find left_attr and right_attr in the relations and get their value count
+		float left_value_count = findAttribute(left_attr, left_attributes).getValueCount();
+		float right_value_count = findAttribute(right_attr, right_attributes).getValueCount();
+
+		// tuple count = T(R)T(S)/max(V(R,A),V(S,B))
+		float max_value = Math.max(left_value_count, right_value_count);
+		float tuple_count = left_tuple_count * right_tuple_count/max_value;
+		Relation output = new Relation((int) tuple_count);
+
+		// value count = min(V(R, A), V(S, B))
+		float min_value = Math.min(left_value_count, right_value_count);
+		// check if value count is less than tuple count
+		min_value = Math.min(tuple_count, min_value);
+		// create the attributes with appropriate value counts
+		Attribute attr1 = new Attribute(left_attr.getName(), (int) min_value);
+		Attribute attr2 = new Attribute(right_attr.getName(), (int) min_value);
+
+		// add the rest of the attributes to the relation
+		for(int i = 0; i < left_attributes.size(); i++) {
+			if(!left_attributes.get(i).equals(left_attr)){
+				output.addAttribute(left_attributes.get(i));
+			}
+		}
+		output.addAttribute(attr1);
+		for(int i = 0; i < right_attributes.size(); i++) {
+			if(!right_attributes.get(i).equals(right_attr)){
+				output.addAttribute(left_attributes.get(i));
+			}
+		}
+		output.addAttribute(attr2);
+
 		op.setOutput(output);
-		//	(new Inspector()).visit(op);
+		(new Inspector()).visit(op);
 	}
 }
